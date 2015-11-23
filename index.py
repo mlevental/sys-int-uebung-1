c25ea0aa86b4e9abd367c4b56998f8ca26b72a44
mlevental <m.levental@web.de>
2015-11-24 00:14:10 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,32 @@
+from bottle import route, run, static_file, template, redirect
+import mb
+
+
+@route('/ping')
+def test():
+    return "Ping successful!"
+
+
+@route('/')
+def redirect_to_show_mb():
+    redirect('/Mandelbrot/getMandelbrot/512/512/10')
+
+
+@route('/Mandelbrot/getMandelbrot/<w:int>/<h:int>/<it:int>')
+def show_mb(w, h, it):
+    wMin, hMin, itMin = 1, 1, 1
+    wMax, hMax, itMax = 800, 600, 1000
+
+    if (w < wMin or w > wMax) or (h < hMin or h > hMax) or (it < itMin or it > itMax):
+        return template('error.html', w=w, h=h, it=it, wMin=wMin, wMax=wMax, hMin=hMin, hMax=hMax, itMin=itMin, itMax=itMax)
+    else:
+        mb.create_mb(w, h, it)
+        return template('index.html', mb='/mb.png', w=w, h=h, it=it)
+
+
+@route('/<mb>')
+def show_mb(mb):
+    return static_file(mb, root=".")
+
+
+run(host='0.0.0.0', port=8080, debug=True)
